c0531e44dae010ed598544716a4d1e1d31226e55
Ava Baker <ava.baker@duke.edu>
2025-02-21 18:09:19 +0800

    added config folder and installed pyupdater

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # imports
 import sys
-import os
 import logging
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
+from pyupdater.client import Client
+from pathlib import Path
 
 from src.controllers.dbcontroller import db_controller
 from src.controllers.pathcontroller import resource_path
@@ -14,8 +15,18 @@ def runApp():
         window = MainWindow()
         app.exec_()
 
+def check_for_updates():
+    client = Client('bullet', Path.home() / '.pyupdater')
+    client.refresh()
+    app_update = client.update_check('bullet', '1.0.0')  # Replace with your version
+    if app_update:
+        app_update.download()
+        if app_update.is_downloaded():
+            app_update.extract_restart()
+
 if __name__ == '__main__':
     # run the app
+    check_for_updates
     try:
         app = QApplication(sys.argv)
         app.setApplicationName("bullet")
